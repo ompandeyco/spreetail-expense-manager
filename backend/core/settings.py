870a5833fe91d32ae8c2ead5a51666dcb7fd05a1
ompandeyin <omompandey7@gmail.com>
2026-06-15 15:30:04 +0530

    Prepare Django backend deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -158,3 +159,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",   # Vite default dev port
     "http://localhost:3000",
 ]
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
